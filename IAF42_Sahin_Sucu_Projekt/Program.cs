2aa196e468f9148ae8eefc20b9fa25575d8e3fdf
Ozan <aykan.s2@gso.schule.koeln>
2026-06-08 21:22:51 +0200

    Hallo

--- IAF42_Sahin_Sucu_Projekt/Program.cs
+++ IAF42_Sahin_Sucu_Projekt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace IAF42_Sahin_Sucu_Projekt
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hallo");
         }
     }
 }
