14a7d1c5dae31daa772bcf1fb68f8911bfa54d6c
aykan sucu <aykan.s2so.schule.koeln>
2026-06-08 14:35:11 +0200

    Test

---
+++ IAF42_Sahin_Sucu_Projekt/Rechteck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAF42_Sahin_Sucu_Projekt
+{
+    internal class Rechteck
+    {
+        private int groeße;
+    }
+}
